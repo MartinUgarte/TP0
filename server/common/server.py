c8da5b4155863798e3affdb4473c15b6084e5aac
MartinUgarte <martinugarte2012@gmail.com>
2024-03-23 22:28:11 -0300

    get winners

--- server/common/server.py
+++ server/common/server.py
@@ -4,10 +4,11 @@ import signal
 
 from .connection import ClientConnection
 
-from .utils import Bet, store_bets
+from .utils import load_bets, has_won
 
 BET_SEPARATOR = "\t"
 ALL_BETS_ACK = "ALL_BETS_ACK"
+AGENCIES = 5
 
 class Server:
     def __init__(self, port, listen_backlog):
@@ -15,6 +16,7 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._client_conns = []
         self.active = True
 
     def run(self):
@@ -32,6 +34,7 @@ class Server:
             client_sock = self.__accept_new_connection()
             if self.active:
                 client_conn = ClientConnection(client_sock, client_sock.getpeername())
+                self._client_conns.append(client_conn)
                 self.__handle_client_connection(client_conn)
         
     def __handle_sigterm(self, signal, frame):
@@ -42,6 +45,30 @@ class Server:
         self.active = False
         self._server_socket.close()
 
+    def __receive_bets(self, client_conn):
+        try:
+            client_conn.receive_messages()    
+            logging.info(f'action: receive_all_bets | result: success | ip: {client_conn.client_addr[0]}')   
+
+            if not client_conn.send_message(ALL_BETS_ACK): return   
+            logging.info(f'action: send_all_bets_ack | result: succes | ip: {client_conn.client_addr[0]}')
+
+            if len(self._client_conns) == AGENCIES:
+                return True
+
+        except OSError as e:
+            logging.error(f'action: receive_message | result: fail | error: {e}')
+            client_conn.close()
+            return False
+
+    def __find_winners(self):
+        winners = []
+        for bet in load_bets():
+            if has_won(bet):
+                winners.append(bet)
+        for winner in winners:
+            logging.info(f"action: get_winners | result: success | winner: {winner.first_name} {winner.last_name}")
+
     def __handle_client_connection(self, client_conn):
         """
         Read message from a specific client socket and closes the socket
@@ -50,19 +77,10 @@ class Server:
         client socket will also be closed
         """
 
-        try:
-            client_conn.receive_messages()    
-            logging.info(f'action: receive_all_bets | result: success | ip: {client_conn.client_addr[0]}')   
-
-            if not client_conn.send_message(ALL_BETS_ACK): return   
-            logging.info(f'action: send_all_bets_ack | result: succes | ip: {client_conn.client_addr[0]}')
-
-        except OSError as e:
-            logging.error(f'action: receive_message | result: fail | error: {e}')
-
-        finally:
-            logging.info(f'action: close_connection | ip: {client_conn.client_addr[0]}')
-            client_conn.close()
+        if not self.__receive_bets(client_conn):
+            return
+        
+        self.__find_winners()
             
     def __accept_new_connection(self):
         """
